f9494cc6a2f89400bbdc66de1ef4d138f9d5bfc9
Madt5 <alenov05022007@gmail.com>
2025-03-29 11:05:37 +0500

    #4

---
+++ src/Asik1/ex4.java
@@ -0,0 +1,15 @@
+package Asik1;
+
+public class ex4 {
+    public static int fact(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * fact(n - 1);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+}
